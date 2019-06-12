101b4862e17bfcedcc1d6ad43ee826a8977983dd
Zachary Geier <zachary.d.geier@accenturefederal.com>
2019-06-12 16:28:47 -0400

    no-nittiwts

--- no-nittwits.py
+++ no-nittwits.py
@@ -1,24 +1,36 @@
 import webbrowser
-
 import tweepy
 
-if __name__ == "__main__":
-
-    consumer_key = raw_input('Consumer key: ').strip()
-    consumer_secret = raw_input('Consumer secret: ').strip()
+def login():
+    consumer_key = input('Consumer key: ').strip()
+    consumer_secret = input('Consumer secret: ').strip()
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-    # Open authorization URL in browser
     webbrowser.open(auth.get_authorization_url())
 
-    # Ask user for verifier pin
-    pin = raw_input('Verification pin number from twitter.com: ').strip()
+    pin = input('Verification pin number from twitter.com: ').strip()
 
-    # Get access token
     token = auth.get_access_token(verifier=pin)
+    api = tweepy.API(auth)
 
-    # Give user the access token
-    print 'Access token:'
-    print '  Key: %s' % token.key
-    print '  Secret: %s' % token.secret
+    return api, token
 
+def pick_a_list(api, user):
+    user_lists = api.lists_all(user.screen_name)
+    for i, user_list in enumerate(user_lists):
+        print(i, user_list.name)
+
+    list_num = int(input('Enter the number of the group that you want to follow: '))
+
+    return user_lists[list_num]
+
+def follow_list(myuser, user_list):
+    for member in api.list_members(owner_screen_name=myuser.screen_name, slug=user_list.slug):
+        api.create_friendship(member.screen_name)
+
+api, token = login()
+
+user = api.get_user(input("Username of the account with the list: "))
+selected_list = pick_a_list(api, user)
+
+follow_list(user, selected_list)

b781c1b6a9cf46192c3e24e0216b567082b5ed14
Zachary Geier <zachary.d.geier@accenturefederal.com>
2019-06-12 12:34:01 -0400

    pretty good results

--- tasmania/tasmania.py
+++ tasmania/tasmania.py
@@ -14,41 +14,40 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-# Load dataset
+# Read in data from csv
 training_dataset_path = ".\\contest-train.csv"
 target_dataset_path = ".\\contest-test.csv"
 
-cols_to_drop = ['Unnamed: 0', 'ID', 'Course Name', 'Course URL / Code', 'Certification URL','Consolidated Course Name', 'Assigned To', 'Request Status', 'Start Date', 'End Date', 'Start Mo/Yr', 'End Mo/Yr', 'Start FY', 'End FY', 'Individual Travel Hours', 'Rqst Tot Labor Hrs', 'Airfare', 'Hotel', 'Per Diem', 'Other', 'Estimated Individual Travel', 'Misc Expenses', 'Catering', 'Facility Rental', 'Direct Other Expenses', 'Describe Other Expenses', 'Direct Expense Impact', 'Rqst NPR Alloc', 'Rqst NPR OH', 'Cancel No Response', 'Created', 'Retroactive Start Date', 'Duplicates', 'Reporting Status']
+train_base = pd.read_csv(training_dataset_path, encoding='Latin-1', index_col=0)
+target_base = pd.read_csv(target_dataset_path, encoding='Latin-1', index_col=0)
+
+# Drop unrelated columns
+cols_to_drop = ['ID', 'Course Name', 'Course URL / Code', 'Certification URL','Consolidated Course Name', 'Assigned To', 'Request Status', 'Start Date', 'End Date', 'Start Mo/Yr', 'End Mo/Yr', 'Start FY', 'End FY', 'Individual Travel Hours', 'Rqst Tot Labor Hrs', 'Airfare', 'Hotel', 'Per Diem', 'Other', 'Estimated Individual Travel', 'Misc Expenses', 'Catering', 'Facility Rental', 'Direct Other Expenses', 'Describe Other Expenses', 'Direct Expense Impact', 'Rqst NPR Alloc', 'Rqst NPR OH', 'Cancel No Response', 'Created', 'Retroactive Start Date', 'Duplicates', 'Reporting Status']
 categorical_columns = ['Training Source', 'Home Office/Metro Area', 'Organization Number', 'Organization', 'Capability', 'Function 2', 'Career Level', 'Function', 'Function Name', 'Title','Training Type', 'Training Provider', 'Training Delivery Type', 'Training Location', 'Vendor Name', 'Conference Name', 'Course or Event Name', 'Certification Type', 'Certification Name', 'Is there a course with this certification?', 'Activity', 'Support Group', 'Business Justification', 'What % of the conference is business development?', 'Travel Required']
 
-train_base = pd.read_csv(training_dataset_path, encoding='Latin-1')
-train_base = train_base.drop(cols_to_drop, axis=1)
-#target_base = pd.read_csv(target_dataset_path, encoding='Latin-1')
+combined_base = pd.concat([train_base, target_base], axis=0).drop(cols_to_drop, axis=1)
+combined_dummies = pd.get_dummies(data = combined_base, columns = categorical_columns, drop_first = True)
 
-#combined_base = pd.concat([train_base, target_base], axis=0)
-#combined_dummies = pd.get_dummies(data = combined_base, columns = categorical_columns, drop_first = True)
+# Get rows with the category column filled out (training set)
+combined_with_category = combined_dummies.dropna(subset=['Category'])
+X = combined_with_category.drop(columns='Category')
+Y = combined_with_category['Category'].copy()
 
-#df = pd.dropna(subset=['Category'])
-df = pd.get_dummies(data = train_base, columns = categorical_columns, drop_first = True)
-#df = combined_dummies.dropna(subset=['Category'])
+train, test, train_labels, test_labels =  train_test_split(X, Y, test_size = .02)
 
-X = df.drop(columns='Category')
-Y = df['Category'].copy()
-
-#split the training portion - setting very low for final pass, use .2 for reasonable accuracy test
-train, test, train_labels, test_labels =  train_test_split(X, Y, test_size = .02, random_state=42)
-
-print(test.head())
-
-lr = LogisticRegression(C=10)
+# I got the best results with LR
+lr = LogisticRegression(solver='liblinear', multi_class='auto')
 lr_model = lr.fit(train, train_labels)
 lr_preds = lr.predict(test)
-print ("LR: ", accuracy_score(test_labels, lr_preds))
+print ("Logistic Regression Accuracy: ", accuracy_score(test_labels, lr_preds))
 
-#target_base = pd.read_csv(target_dataset_path, encoding='Latin-1')
-#target_base = target_base.drop(cols_to_drop, axis=1)
-#target_base = pd.get_dummies(data = target_base, columns = categorical_columns, drop_first = True)
-#target_test = target_base.drop(columns='Category')
+# Test out the model on the target dataset
+target_test = combined_dummies[combined_dummies.Category.isnull()]
+target_test = target_test.drop(['Category'], axis = 1)
 
-#print(target_test.head())
-#target_output_categories = lr.predict(target_test)  
+predictions_test = lr.predict(target_test)
+
+for i, prediction in enumerate(predictions_test):
+    target_base.loc[i, 'Category'] = prediction
+
+target_base.to_csv('results.csv')
